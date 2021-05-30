9e479db2362e81fdd599a8d23e686f76b5ff116f
slezakt <info@tomasslezak.cz>
2021-05-30 15:07:11 +0200

    pokus

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,26 @@
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
       <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
-      <h2 class="subtitle text-center">Boilerplate</h2>
-    </section>
-
-    <section class="mt-8">
-      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest blog post</h3>
-      <posts post-type="blog" :amount="1" />
+      <nuxt-content :document="page" />
     </section>
   </main>
 </template>
+<script>
+import Logo from '~/components/Logo'
+export default {
+  async asyncData({ $content, error, params }) {
+    let page;
+    try {
+      page = await $content("about").fetch();
+    } catch (e) {
+      error({ message: "Blog posts not found" });
+    }
+    return { page };
+  },
+}
+</script>
+
+
+
+
+
